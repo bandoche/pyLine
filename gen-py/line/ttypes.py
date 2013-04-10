09ba2ca8223992040527ab4ff35b7a74d137d00b
Sang-jun Jung <bandoche@hanmail.net>
2013-04-10 21:36:49 +0900

    changed thrift

--- gen-py/line/ttypes.py
+++ gen-py/line/ttypes.py
@@ -160,3 +160,87 @@ class loginWithIdentityCredentialForCertificateResult:
 
   def __ne__(self, other):
     return not (self == other)
+
+class loginWithIdentityCredentialForCertificateResult2:
+  """
+  Attributes:
+   - keepLoggedIn
+   - systemName
+   - certificate
+  """
+
+  thrift_spec = (
+    None, # 0
+    (1, TType.BOOL, 'keepLoggedIn', None, None, ), # 1
+    (2, TType.I32, 'systemName', None, None, ), # 2
+    (3, TType.STRING, 'certificate', None, None, ), # 3
+  )
+
+  def __init__(self, keepLoggedIn=None, systemName=None, certificate=None,):
+    self.keepLoggedIn = keepLoggedIn
+    self.systemName = systemName
+    self.certificate = certificate
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 1:
+        if ftype == TType.BOOL:
+          self.keepLoggedIn = iprot.readBool();
+        else:
+          iprot.skip(ftype)
+      elif fid == 2:
+        if ftype == TType.I32:
+          self.systemName = iprot.readI32();
+        else:
+          iprot.skip(ftype)
+      elif fid == 3:
+        if ftype == TType.STRING:
+          self.certificate = iprot.readString();
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('loginWithIdentityCredentialForCertificateResult2')
+    if self.keepLoggedIn is not None:
+      oprot.writeFieldBegin('keepLoggedIn', TType.BOOL, 1)
+      oprot.writeBool(self.keepLoggedIn)
+      oprot.writeFieldEnd()
+    if self.systemName is not None:
+      oprot.writeFieldBegin('systemName', TType.I32, 2)
+      oprot.writeI32(self.systemName)
+      oprot.writeFieldEnd()
+    if self.certificate is not None:
+      oprot.writeFieldBegin('certificate', TType.STRING, 3)
+      oprot.writeString(self.certificate)
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    return
+
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
--- line.thrift
+++ line.thrift
@@ -13,9 +13,16 @@ struct loginWithIdentityCredentialForCertificateResult {
   3: string certificate 
 }
 
+struct loginWithIdentityCredentialForCertificateResult2 {
+  1: bool keepLoggedIn,
+  2: i32 systemName,
+  3: string certificate 
+}
+
 service Line {
 	#string loginWithIdentityCredentialForCertificate(1:3:string cr1, 4:string cr2, 5:bool flag1, 6:string ip, 7:string comname, 8: i32 val1 9:string cr5)
 	#Unknown testFunc()
 	loginWithIdentityCredentialForCertificateResult loginWithIdentityCredentialForCertificate(1:string cr1, 2:string cr2, 3:bool flag1, 4:string ip, 5:string comname, 6: i32 val1 7:string cr5)
+	loginWithIdentityCredentialForCertificateResult2 loginWithIdentityCredentialForCertificate2(1:string cr1, 2:string cr2, 3:bool flag1, 4:string ip, 5:string comname, 6: i32 val1 7:string cr5)
 }
 
