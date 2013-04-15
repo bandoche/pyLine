c34584012da2e22e968f3140486b3676baf35c45
Sang-jun Jung <bandoche@hanmail.net>
2013-04-15 15:19:13 +0900

    update line.thrift

--- line.thrift
+++ line.thrift
@@ -1,28 +1,86 @@
-const string HELLO_IN_KOREAN = "an-nyoung-ha-se-yo"
-const string HELLO_IN_FRENCH = "bonjour!"
-const string HELLO_IN_JAPANESE = "konichiwa!"
+#method name :  loginWithIdentityCredentialForCertificate
+#@ 0 # 0 - STRUCT
+#@ 2 # 1 - STRING ( 88 ) =  DsKeAAAAAAAAAAAAAAAA.AAAAAAAAAAAAAAAAAAAAAA.AAAAAAAAAAA+a/AA+AAAAAAAAAAAAAAAAAAAAAAAAAA=
+#@ 92 # 5 - I32 (1) =  2
+#@ 94 # 0 - STOP
 
+#at all flow init 
+#method name :  loginWithVerifierForCertificate
+#@ 0 # 0 - STRUCT
+#@ 2 # 1 - STRING ( 88 ) =  DsKeAAAAAAAAAAAAAAAA.AAAAAAAAAAAAAAAAAAAAAA.AAAAAAAAAAA+a/AA+AAAAAAAAAAAAAAAAAAAAAAAAAA=
+#@ 92 # 2 - STRING ( 64 ) =  5d7b7BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+#@ 158 # 5 - I32 (1) =  2
+#@ 160 # 0 - STOP
 
-struct Unknown {
-  1: i32 temp
-}
+#method name :  loginWithIdentityCredentialForCertificate
+#@ 0 # 0 - STRUCT
+#@ 2 # 3 - STRING ( 32 ) =  rHMIGCCCCCCCCCCCCCCCCCCCCCCCCCCC
+#@ 36 # 4 - STRING ( 4 ) =  5210
+#@ 42 # 5 - I32 (1) =  6
+#@ 44 # 0 - STOP
 
 struct loginWithIdentityCredentialForCertificateResult {
-  1: bool keepLoggedIn,
-  2: i32 systemName,
-  3: string certificate 
+  1: string line_access, # real X-Line-Access key for communitation. DsKeAAAAAAAAAAAAAAAA.AAAAAAAAAAAAAAAAAAAAAA.AAAAAAAAAAA+a/AA+AAAAAAAAAAAAAAAAAAAAAAAAAA=
+  2: string key64, #unknown. only offered when login with initial state. (need verifier)
+  3: string verifier, # aka. verifier. temp, rHMIGCCCCCCCCCCCCCCCCCCCCCCCCCCC
+  4: string auth_digit, # 5210
+  5: i32 code # 6: need auth? 2: correct login
 }
 
-struct loginWithIdentityCredentialForCertificateResult2 {
-  1: bool keepLoggedIn,
-  2: i32 systemName,
-  3: string certificate 
-}
+
+
+
+
+
+
+
+
+
+
 
 service Line {
 	#string loginWithIdentityCredentialForCertificate(1:3:string cr1, 4:string cr2, 5:bool flag1, 6:string ip, 7:string comname, 8: i32 val1 9:string cr5)
-	#Unknown testFunc()
-	loginWithIdentityCredentialForCertificateResult loginWithIdentityCredentialForCertificate(1:string cr1, 2:string cr2, 3:bool flag1, 4:string ip, 5:string comname, 6: i32 val1 7:string cr5)
-	loginWithIdentityCredentialForCertificateResult2 loginWithIdentityCredentialForCertificate2(1:string cr1, 2:string cr2, 3:bool flag1, 4:string ip, 5:string comname, 6: i32 val1 7:string cr5)
+
+
+
+#method name :  loginWithIdentityCredentialForCertificate
+#@ 0 # 3 - STRING ( 9 ) =  100007673
+#@ 11 # 4 - STRING ( 202 ) =  1dcAAA202AAADDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+#@ 216 # 5 - BOOL bool - false
+#@ 217 # 6 - STRING ( 13 ) =  172.20.100.30
+#@ 232 # 7 - STRING ( 8 ) =  comnames
+#@ 242 # 8 - I32 (1) =  4
+#@ 244 # 9 - STRING ( 64 ) =  5d7b7BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+#@ 310 # 0 - STOP
+
+#@ 0 # 3 - STRING ( 9 ) =  100007672
+#@ 11 # 4 - STRING ( 202 ) =  1dcAAA202AAADDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+#@ 216 # 5 - BOOL bool - false
+#@ 217 # 6 - STRING ( 11 ) =  172.20.10.3
+#@ 230 # 7 - STRING ( 8 ) =  comnames
+#@ 240 # 8 - I32 (1) =  4
+#@ 242 # 9 - STRING (0)
+
+  loginWithIdentityCredentialForCertificateResult loginWithIdentityCredentialForCertificate
+  (1:string str1, 
+    2:string str2, 
+    3:string rsakey1, 
+    4:string rsacipher, 
+    5:bool b5, 
+    6:string ip, 
+    7:string comname, 
+    8:i32 i8, 
+    9:string str9) 
+#  loginWithIdentityCredentialForCertificateResult loginWithIdentityCredentialForCertificate(1:string cr1, 2:string cr2, 3:bool flag1, 4:string ip, 5:string comname, 6: i32 val1 7:string cr5)
+
+
+
+
+
+#method name :  loginWithVerifierForCertificate
+#@ 0 # 3 - STRING ( 32 ) =  rHMIGCCCCCCCCCCCCCCCCCCCCCCCCCCC
+#@ 34 # 0 - STOP
+  loginWithIdentityCredentialForCertificateResult loginWithVerifierForCertificate(3: string verifier)
+
 }
 
